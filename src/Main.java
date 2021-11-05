3ddf430cc435d14c22adb05d87c5f6076e1ed3b0
mrsimonovets <ilyasimonovec@mail.ru>
2021-11-05 10:36:28 +0300

    second Task: adding three methods instead of make Coffee

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,43 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
+        System.out.println("What coffee do you want?");
+        System.out.println("1. Latte, 2. Cappuccino, 3. Raf");
+        int number2 = sc.nextInt();
+
         if (number == 1){
             NewYorkCoffeeShop c1 = new NewYorkCoffeeShop();
-            c1.makeCoffee();
+            if (number2 == 1){
+                c1.makeLatte();
+            } else if (number2 == 2){
+                c1.makeCappuccino();
+            } else if (number2 == 3){
+                c1.makeRaf();
+            } else {
+                System.out.println("Check your number, please.");
+            }
         } else if (number == 2){
             MoscowCoffeeShop c1 = new MoscowCoffeeShop();
-            c1.makeCoffee();
+            if (number2 == 1){
+                c1.makeLatte();
+            } else if (number2 == 2){
+                c1.makeCappuccino();
+            } else if (number2 == 3){
+                c1.makeRaf();
+            } else {
+                System.out.println("Check your number, please.");
+            }
         } else if (number == 3){
             MinskCoffeeShop c1 = new MinskCoffeeShop();
-            c1.makeCoffee();
+            if (number2 == 1){
+                c1.makeLatte();
+            } else if (number2 == 2){
+                c1.makeCappuccino();
+            } else if (number2 == 3){
+                c1.makeRaf();
+            } else {
+                System.out.println("Check your number, please.");
+            }
         } else{
             System.out.println("Check your number, please.");
         }
@@ -23,19 +51,37 @@ public class Main {
 }
 
 class NewYorkCoffeeShop {
-    public void makeCoffee(){
-        System.out.println("Thanks for order, take your coffee. Bye!");
+    public void makeLatte(){
+        System.out.println("Thanks for order in New York, take your Latte. Bye!");
+    }
+    public void makeCappuccino(){
+        System.out.println("Thanks for order in New York, take your Cappuccino. Bye!");
+    }
+    public void makeRaf(){
+        System.out.println("Thanks for order in New York, take your Raf. Bye!");
     }
 }
 
 class MoscowCoffeeShop {
-    public void makeCoffee(){
-        System.out.println("Thanks for order, take your coffee. Bye!");
+    public void makeLatte(){
+        System.out.println("Thanks for order in Moscow, take your Latte. Bye!");
+    }
+    public void makeCappuccino(){
+        System.out.println("Thanks for order in Moscow, take your Cappuccino. Bye!");
+    }
+    public void makeRaf(){
+        System.out.println("Thanks for order in Moscow, take your Raf. Bye!");
     }
 }
 
 class MinskCoffeeShop {
-    public void makeCoffee(){
-        System.out.println("Thanks for order, take your coffee. Bye!");
+    public void makeLatte(){
+        System.out.println("Thanks for order in Minsk, take your Latte. Bye!");
+    }
+    public void makeCappuccino(){
+        System.out.println("Thanks for order in Minsk, take your Cappuccino. Bye!");
+    }
+    public void makeRaf(){
+        System.out.println("Thanks for order in Minsk, take your Raf. Bye!");
     }
 }
