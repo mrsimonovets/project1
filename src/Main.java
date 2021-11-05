fddefc87b90b53b0a754b5dedf1336eb7244e6bf
mrsimonovets <ilyasimonovec@mail.ru>
2021-11-05 11:07:13 +0300

    another type of fourth Task

--- src/Main.java
+++ src/Main.java
@@ -8,51 +8,21 @@ public class Main {
         int number = sc.nextInt();
 
         System.out.println("What coffee do you want?");
-        System.out.println("1. Latte, 2. Cappuccino, 3. Raf");
+        System.out.println("1. Latte, 2. Cappuccino, 3. Flat White");
         int number2 = sc.nextInt();
 
         if (number == 1){
-            NewYorkCoffeeShop c1 = new NewYorkCoffeeShop("New York");
-            if (number2 == 1){
-                c1.waitCoffee();
-                c1.makeLatte();
-            } else if (number2 == 2){
-                c1.waitCoffee();
-                c1.makeCappuccino();
-            } else if (number2 == 3){
-                c1.waitCoffee();
-                c1.makeRaf();
-            } else {
-                System.out.println("Check your number, please.");
-            }
+            NewYorkCoffeeShop c1 = new NewYorkCoffeeShop("New York",  number2);
+            c1.waitCoffee();
+            c1.makeCoffee();
         } else if (number == 2){
-            MoscowCoffeeShop c1 = new MoscowCoffeeShop("Moscow");
-            if (number2 == 1){
-                c1.waitCoffee();
-                c1.makeLatte();
-            } else if (number2 == 2){
-                c1.waitCoffee();
-                c1.makeCappuccino();
-            } else if (number2 == 3){
-                c1.waitCoffee();
-                c1.makeRaf();
-            } else {
-                System.out.println("Check your number, please.");
-            }
+            MoscowCoffeeShop c1 = new MoscowCoffeeShop("Moscow",  number2);
+            c1.waitCoffee();
+            c1.makeCoffee();
         } else if (number == 3){
-            MinskCoffeeShop c1 = new MinskCoffeeShop("Minsk");
-            if (number2 == 1){
-                c1.waitCoffee();
-                c1.makeLatte();
-            } else if (number2 == 2){
-                c1.waitCoffee();
-                c1.makeCappuccino();
-            } else if (number2 == 3){
-                c1.waitCoffee();
-                c1.makeRaf();
-            } else {
-                System.out.println("Check your number, please.");
-            }
+            MinskCoffeeShop c1 = new MinskCoffeeShop("Minsk",  number2);
+            c1.waitCoffee();
+            c1.makeCoffee();
         } else{
             System.out.println("Check your number, please.");
         }
@@ -61,42 +31,53 @@ public class Main {
 
 class CoffeeHouse {
     String city;
+    int typeOfCoffee;
 
-    public CoffeeHouse(String city){
+    public CoffeeHouse(String city, int typeOfCoffee){
         this.city = city;
+        this.typeOfCoffee = typeOfCoffee;
     }
 
-    public void makeLatte(){
-        System.out.printf("Thanks for order in %s, take your Latte. Bye!", city);
+    public void makeCoffee(){
+        if (typeOfCoffee == 1){
+            System.out.printf("Thanks for order in %s, take your Latte. Bye!", city);
+        } else if (typeOfCoffee == 2){
+            System.out.printf("Thanks for order in %s, take your Cappuccino. Bye!", city);
+        } else if (typeOfCoffee == 3) {
+            System.out.printf("Thanks for order in %s, take your Flat White. Bye!", city);
+        } else {
+            System.out.println("Check your number, please.");
+        }
     }
 
-    public void makeCappuccino(){
-        System.out.printf("Thanks for order in %s, take your Cappuccino. Bye!", city);
-    }
-
-    public void makeRaf(){
-        System.out.printf("Thanks for order in %s, take your Raf. Bye!", city);
-    }
     public void waitCoffee(){
         System.out.println("Please, wait a minute.");
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
 
 class NewYorkCoffeeShop extends CoffeeHouse{
-    public NewYorkCoffeeShop(String city){
-        super(city);
+    public NewYorkCoffeeShop(String city, int typeOfCoffee){
+        super(city, typeOfCoffee);
     }
 }
 
 class MoscowCoffeeShop extends CoffeeHouse{
-    public MoscowCoffeeShop(String city){
-        super(city);
+    public MoscowCoffeeShop(String city, int typeOfCoffee){
+        super(city, typeOfCoffee);
     }
 }
 
 class MinskCoffeeShop extends CoffeeHouse{
-    public MinskCoffeeShop(String city){
-        super(city);
+    public MinskCoffeeShop(String city, int typeOfCoffee){
+        super(city, typeOfCoffee);
     }
 }
